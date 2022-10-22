ea507b90a831b561a3a555182cf3dfccb1bb7bc5
natalia <natalia@localhost.localdomain>
2022-10-22 17:36:16 -0500

    Escritura funcion 1. Idea para distribuir

--- Practica01/primes/lib/primesFinder.ex
+++ Practica01/primes/lib/primesFinder.ex
@@ -4,21 +4,30 @@ defmodule PrimesFinder do
     primes = Server.start(10,upper_bound)
     finish_time = :os.system_time(:millisecond)
 
-    write_primes(primes)
-    write_time(start_time,finish_time)
+    diff_time = finish_time - start_time
+    time_text = "\nInicio: "<>Integer.to_string(start_time)<>"  Final: "<>Integer.to_string(finish_time)<>"  Diferencia: "<>Integer.to_string(diff_time)
   end
 
-  def write_primes(primes) do
-    file_primes = File.open!("primes.txt",[:write])
-    IO.write(file_primes,inspect(primes, pretty: true, limit: :infinity))
-    File.close(file_primes)
-  end
-
-  def write_time(start_time,finish_time) do
+  def write_time do
     file_time = File.open!("times.txt",[:write])
-    diff_time = start_time-finish_time
-    time_text = "Inicio: "<>Integer.to_string(start_time)<>"  Final: "<>Integer.to_string(finish_time)<>"  Diferencia: "<>Integer.to_string(diff_time)
+    time = 100..1000
+    |> Enum.map(fn n -> range(n) end)
 
-    IO.binwrite(file_time,time_text)
+    time = List.to_charlist(time)
+    IO.write(file_time,time)
+  end
+
+  def dispenser(upper_bound) do
+    lider = Process.spawn((fn x -> 2 end), [:link])
+    manage(upper_bound)                             
+  end
+
+  def manage(upper_bound) do
+    block = div(upper_bound, 10)
+    0..9
+    |> Enum.to_list()
+    |> Enum.map(fn wid -> Worker.start((wid * block) + 1, min((wid + 1) * block, upper_bound)) end)
+    |> Enum.map(fn x -> Process.info(x) end)
   end
 end
+  
