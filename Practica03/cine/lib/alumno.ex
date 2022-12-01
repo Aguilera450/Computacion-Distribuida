c9c5caa51ec61bc56c9c5e97f16d8204c67fd8ad
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2022-11-30 19:52:25 -0600

    Practica 3 y archivo para resumen

--- Practica03/cine/lib/alumno.ex
+++ Practica03/cine/lib/alumno.ex
@@ -6,13 +6,15 @@ defmodule Alumno do
     end)
   end
 
-  def envia_mensaje(caller) do
+  def envia_mensaje(caller,cont) do
+    n=elige_numero()
+    cont=cont+1
     #Probabilidad de fallar
     if Enum.random(1..4) == 3 do
+      IO.puts("El proceso #{inspect(caller)} propone #{inspect(n)}, antes de fallar")
       Process.exit(caller, :kill)
     else
-      n=elige_numero()
-      #IO.inspect(n)
+      IO.puts("El proceso #{inspect(caller)} propone #{inspect(n)}, #{cont} veces")
       send(caller,n)
     end
   end
@@ -24,18 +26,27 @@ defmodule Alumno do
   def recibe_propuesta do
     IO.inspect("Estoy esperando mensajes")
     receive do
+      {:prop,n} -> IO.puts("Se recibe la propuesta #{n}")
+                    n
       {:vista_props,list}-> Alumno.elige_min(list)
-      {:prop,n} -> n
-      {:prop_min,n,l} -> IO.puts("propuesta=#{inspect(l)}")
-      _ -> IO.inspect("Alumno: Otro mensaje")
+      n->n
     end
-    Process.sleep(100)
+    Process.sleep(1000)
   end
   def elige_min(lista_prop) do
     l=Enum.sort(lista_prop, :asc)
     l=Enum.filter(l,fn x-> x != true end)
     IO.puts("v=#{inspect(l)}")
     tmp = List.first(l)
+
     send(self(),{:prop_min,tmp,l})
+  end
+
+  def envia_mensaje_n(n,cont) do
+    caller=self()
+    IO.puts("El proceso propone #{inspect(n)}, #{cont} veces")
+    send(caller,n)
+    Process.sleep(1000)
+    n
   end
 end
--- Practica03/cine/lib/lider.ex
+++ Practica03/cine/lib/lider.ex
@@ -1,27 +1,45 @@
 defmodule Lider do
   def start(n_alumnos) do
-    list = for _x <- 1..n_alumnos do
-            caller = Alumno.start()
-            Alumno.envia_mensaje(caller)
-          end
-    send(self(),{:vista_props,list})
+    cont=0
+    list=for _x <- 1..n_alumnos do
+      caller = Alumno.start()
+      Alumno.envia_mensaje(caller,cont)
+      end
 
+    list=Enum.filter(list,fn x-> x != true end)
+    send(self(),{:vista_props,list})
     loop(n_alumnos)
   end
 
   defp loop(n_alumnos) do
     Alumno.recibe_propuesta()
-    Process.sleep(1000)
     receive do
       {:prop_min,n,l} -> terminar(n,l,n_alumnos)
-      _ -> loop(n_alumnos)
     end
+    Process.sleep(1000)
   end
   def terminar(n,l,n_alumnos) do
-    IO.puts("El total de procesos que participaron es #{inspect(n_alumnos)}")
-    IO.puts("El acuerdo es #{inspect(n)}")
-    l=Enum.filter(l,fn x-> x != true end)
-    size = length(l)
-    IO.puts("Numero de procesos que terminaron el algoritmo #{inspect(size)}")
+    if length(l) >= n_alumnos do
+      IO.puts("No es posible llegar a un consenso")
+    else
+      anuncia_decision(l)
+      Alumno.recibe_propuesta()
+      IO.puts("El total de procesos que participaron es #{inspect(n_alumnos)}")
+      IO.puts("El acuerdo es #{inspect(n)}")
+      l=Enum.filter(l,fn x-> x != true end)
+      size = length(l)
+      IO.puts("Numero de procesos que terminaron el algoritmo #{inspect(size)}")
+    end
   end
+
+  def anuncia_decision(l) do
+    cont=1
+    l = for i <- l do
+              Alumno.envia_mensaje_n(i,cont+1)
+            end
+    l=Enum.filter(l,fn x-> x != true end)
+
+    send(self(),{:vista_props,l})
+  end
+
 end
